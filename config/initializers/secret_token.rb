9d3d4ad8fc097417fc043113812948769d7dce2e
Mazembo <mazemb_eddy@yahoo.fr>
2012-01-15 16:24:10 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telamonaholdings::Application.config.secret_token = '25dac0a7cbb7bb41eaee54122fc1501af1d27d41deae027c699d262f99b005e46a22e373f8c34f4486b359927407f6f960a342863da10307726c44566ed59b80'
